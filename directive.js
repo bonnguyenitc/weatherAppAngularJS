324721b920ef74dc483954c15085b74d83e8b44a
Bon <bng22061996@gmail.com>
2018-01-26 22:14:27 +0700

    app

---
+++ directive.js
@@ -0,0 +1,12 @@
+weatherApp.directive('weatherReport', function(){
+    return {
+        restrict : 'E',
+        templateUrl : 'directive/weatherReport.html',
+        replace : true,
+        scope : {
+            weatherDay : "=",
+            convertToDate : "&",
+            dateFormat : "@"
+        }
+    }
+});
---
+++ service.js
@@ -0,0 +1,4 @@
+weatherApp.service('cityService',function(){
+    ct = this;
+    ct.city = 'Ha Noi';
+});
